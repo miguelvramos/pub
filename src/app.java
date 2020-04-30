f614a8d7a50e3497e808ecfcb8564212dfc1c3ed
Ferreira080 <jvfsilva08@gmail.com>
2020-04-30 19:27:31 -0300

    Update app.java
    Menu da classe app pronto

--- src/app.java
+++ src/app.java
@@ -1,2 +1,36 @@
+import javax.swing.JOptionPane;
+
 public class app {
+
+	public static void main(String[] args) {
+		
+		boolean flag = true;
+		
+		while (flag == true) {
+
+			int escolha = Integer.parseInt(JOptionPane.showInputDialog("Opções: \n 1- Adicionar cliente \n 2- Fechar conta \n 3- Sortear cliente \n 4- Sair \n\n"));
+
+			switch (escolha) {
+				case 1:
+					
+					break;
+
+				case 2:
+					
+					break;
+
+				case 3:
+					
+					break;
+
+				case 4:
+					
+					break;
+			
+				default:
+					JOptionPane.showMessageDialog(null,"Opção Inválida!");
+					break;
+			}
+		}
+	}
 }
