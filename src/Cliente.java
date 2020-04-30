340e06ab2faf823dd8c2befa19cfcd990e5607f7
Ferreira080 <jvfsilva08@gmail.com>
2020-04-30 18:57:43 -0300

    Update Cliente.java
    Classe Cliente Finalizada

--- src/Cliente.java
+++ src/Cliente.java
@@ -3,22 +3,18 @@ public class Cliente {
     private String nome;
     private String numeroTelefone;
 
-    /* Get do Nome */
     public String getNome() {
         return nome;
     }
 
-    /* Get do Número */
     public String getNumeroTelefone() {
         return numeroTelefone;
     }
 
-    /* Set do Nome */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    /* Set do Número */
     public void setNumeroTelefone(String numeroTelefone) {
         this.numeroTelefone = numeroTelefone;
     }
