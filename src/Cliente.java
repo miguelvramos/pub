2d6ba883c04d5f122541e8381a41cfbbc81868f7
Miguel Vieira Ramos <miguelvieiraramos02@gmail.com>
2020-05-01 15:49:21 -0300

    tirado o atributo nome, checagem lista vazia, remocao do nó apos sorteio.

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,26 +1,13 @@
 public class Cliente {
 
-    private String nome;
     private String numeroTelefone;
 
-    public Cliente(String nome, String numeroTelefone) {
-        this.nome = nome;
+    public Cliente(String numeroTelefone) {
         this.numeroTelefone = numeroTelefone;
-    }
-
-    public String getNome() {
-        return nome;
     }
 
     public String getNumeroTelefone() {
         return numeroTelefone;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setNumeroTelefone(String numeroTelefone) {
-        this.numeroTelefone = numeroTelefone;
-    }
 }
--- src/app.java
+++ src/app.java
@@ -5,55 +5,50 @@ public class app {
 	public static void main(String[] args) {
 		Lista lista = new Lista();
 		boolean flag = true;
-
-		Cliente cliente1 = new Cliente("Henrique", "3333");
-        No novoNo1 = new No();
-        novoNo1.setCliente(cliente1);
-        lista.inserirNoFim(novoNo1);
-
-        Cliente cliente2 = new Cliente("Miguel", "9999");
-        No novoNo2 = new No();
-        novoNo2.setCliente(cliente2);
-        lista.inserirNoFim(novoNo2);
-
-        Cliente cliente3 = new Cliente("Arthur", "4444");
-        No novoNo3 = new No();
-        novoNo3.setCliente(cliente3);
-        lista.inserirNoFim(novoNo3);
-		
 		while (flag == true) {
-
-			int escolha = Integer.parseInt(JOptionPane.showInputDialog("Opções: \n 1- Adicionar cliente \n 2- Fechar conta \n 3- Sortear cliente \n 4- Sair \n\n"));
-
+			int escolha = Integer.parseInt(JOptionPane.showInputDialog(
+					"Opções: \n 1- Adicionar cliente \n 2- Fechar conta \n 3- Sortear cliente \n 4- Sair \n"));
 			switch (escolha) {
 				case 1:
-					String nomeCliente = JOptionPane.showInputDialog(null, "Digite o nome do cliente");
-					String telefoneCliente = JOptionPane.showInputDialog(null, "Digite o telefone do cliente");
-					No noCliente = new No();
-					noCliente.setCliente(new Cliente(nomeCliente, telefoneCliente));
-					lista.inserirNoFim(noCliente);
-					break;
-
-				case 2:
-					String telefoneClienteRemover = JOptionPane.showInputDialog(null, "Digite o telefone do cliente para dar baixa");
-					if (lista.removerNo(telefoneClienteRemover)) {
-						JOptionPane.showMessageDialog(null, "Cliente removido");
+					String telefoneCliente = JOptionPane.showInputDialog(
+							null, "Digite o telefone do cliente: ");
+					if (lista.buscarNoNumeroTelefone(telefoneCliente) == null) {
+						No noCliente = new No();
+						noCliente.setCliente(new Cliente(telefoneCliente));
+						lista.inserirNoFim(noCliente);
 					} else {
-						JOptionPane.showMessageDialog(null, "Numero não encontrado");
+						JOptionPane.showMessageDialog(null, "Esse número já existe.");
 					}
 					break;
-
-				case 3:
-					Random sortear = new Random();
-					No noClienteSorteado = lista.buscarNoEspecifico(sortear.nextInt(lista.getQuantidade()));
-					Cliente clienteSorteado = noClienteSorteado.getCliente();
-					JOptionPane.showMessageDialog(null, "Cliente sorteado foi o " + clienteSorteado.getNome() + ", telefone: " + clienteSorteado.getNumeroTelefone());
+				case 2:
+					if (lista.getQuantidade() != 0) {
+						String telefoneClienteRemover = JOptionPane.showInputDialog(
+								null, "Digite o telefone do cliente para dar baixa.");
+						if (lista.removerNo(telefoneClienteRemover)) {
+							JOptionPane.showMessageDialog(null, "Cliente removido.");
+						} else {
+							JOptionPane.showMessageDialog(null, "Número não encontrado.");
+						}
+					} else {
+						JOptionPane.showMessageDialog(null, "A lista está vazia.");
+					}
 					break;
-
+				case 3:
+					if (lista.getQuantidade() != 0) {
+						Random sortear = new Random();
+						No noClienteSorteado = lista.buscarNoEspecifico(sortear.nextInt(lista.getQuantidade()));
+						Cliente clienteSorteado = noClienteSorteado.getCliente();
+						JOptionPane.showMessageDialog(
+								null, "O número sorteado foi " +
+										clienteSorteado.getNumeroTelefone());
+						lista.removerNo(clienteSorteado.getNumeroTelefone());
+					} else {
+						JOptionPane.showMessageDialog(null, "A lista está vazia.");
+					}
+					break;
 				case 4:
 					flag = false;
 					break;
-			
 				default:
 					JOptionPane.showMessageDialog(null,"Opção Inválida!");
 					break;
